9686e6738f15927b0bcadd82abf76e0a18fdfe70
Jiang Kun <chinese.jk@gmail.com>
2016-08-11 23:41:44 +0800

    初版

--- index.php
+++ index.php
@@ -1,260 +1,272 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
-// 各数字所代表的颜色如下：
+use GuzzleHttp\Client;
+use Sunra\PhpSimple\HtmlDomParser;
 
-// 字背景颜色范围:40----49
-// 40:黑
-// 41:深红
-// 42:绿
-// 43:黄色
-// 44:蓝色
-// 45:紫色
-// 46:深绿
-// 47:白色
-
-// 字颜色:30-----------39
-// 30:黑
-// 31:红
-// 32:绿
-// 33:黄
-// 34:蓝色
-// 35:紫色
-// 36:深绿
-// 37:白色
-
-function colorize($text, $status)
+class YunTi
 {
-    $out = '';
-    switch ($status) {
-        case "SUCCESS":
-            $out = "[32m"; //Green
-            break;
-        case "FAILURE":
-            $out = "[31m"; //Red
-            break;
-        case "WARNING":
-            $out = "[33m"; //Yellow
-            break;
-        case "NOTE":
-            $out = "[34m"; //Blue
-            break;
-        default:
-            throw new Exception("Invalid status: " . $status);
+    // /Library/Preferences/SystemConfiguration/preferences.plist
+    const SITE_URL = 'https://www.cryptpline.com';
+    const LOGIN_PATH = '/users/sign_in';
+    const SERVERS_PATH = '/admin/servers';
+
+    private $parser;
+    private $client;
+    private $token;
+    private $servers;
+
+    public function __construct()
+    {
+        $this->parser = new HtmlDomParser();
+
+        $options = [
+            'base_uri' => self::SITE_URL,
+            'timeout' => 15,
+            'cookies' => true
+            // 'debug' => true
+        ];
+
+        $this->client = new Client($options);
+
+        date_default_timezone_set('Asia/Shanghai');
     }
-    return chr(27) . "$out" . "$text" . chr(27) . "[0m";
-}
 
-//get cookie
-function get_cookie($url, $cookie)
-{
-    $ch = curl_init();
-    //设置选项，包括URL
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie); //设置Cookie信息保存在指定的文件中
-    //执行并获取HTML文档内容
-    $output = curl_exec($ch);
-    //释放curl句柄
-    curl_close($ch);
-    //打印获得的数据
+    public function run()
+    {
 
-    return $output;
+        if (is_file('servers.txt')) {
+            $text = file_get_contents('servers.txt', 'r') or die('Unable to open file!');
 
-}
+            $this->servers = json_decode($text, true);
 
-//模拟登录
-function login_post($url, $cookie, $post)
-{
-    $curl = curl_init();//初始化curl模块
-    curl_setopt($curl, CURLOPT_URL, $url);//登录提交的地址
-    curl_setopt($curl, CURLOPT_HEADER, 0);//是否显示头信息
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//是否自动显示返回的信息
-    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie);//读取cookie
-    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie); //设置Cookie信息保存在指定的文件中
-    curl_setopt($curl, CURLOPT_REFERER, "https://www.ytpub.com/users/sign_in");   //来路
-    curl_setopt($curl, CURLOPT_POST, 1);//post方式提交
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));//要提交的信息
-    $result = curl_exec($curl);//执行cURL抓取页面内容
-    curl_close($curl);
-    return $result;
-}
+        } else {
 
-//登录成功后获取数据
-function get_content($url, $cookie)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);//读取cookie
-    $result = curl_exec($ch);//执行cURL抓取页面内容
-    curl_close($ch);
-    return $result;
-}
+            if (is_file('auth.txt')) {
+                $text = file_get_contents('auth.txt', 'r') or die('Unable to open file!');
 
+                $auth = json_decode($text, true);
+            } else {
+                $auth = $this->inputAuth();
+            }
 
-function getNeedBetween($kw, $mark1, $mark2)
-{
-    $st = stripos($kw, $mark1);
-    $ed = stripos($kw, $mark2);
-    if (($st == false || $ed == false) || $st >= $ed)
-        return 0;
-    $kw = substr($kw, ($st + strlen($mark1)), ($ed - $st - strlen($mark2) + 1));
-    return $kw;
-}
+            $this->saveAuth($auth);
 
+            $this->login($auth['username'], $auth['password']);
 
-$servers = file_get_contents('servers.txt');
+            $this->servers = $this->getServers();
 
-$servers = json_decode($servers, true);
+            $this->storageServers($this->servers);
+        }
 
+        $this->pingTest($this->servers);
 
-if (!isset($servers['expires']) || $servers['expires'] < time()) {
-
-    $login_url = 'https://www.ytpub.com/users/sign_in';
-
-
-    $cookie = 'cookie.dat';
-    $a = get_cookie($login_url, $cookie);
-    // preg_match_all('/<meta(.*?)csrf-token',$a, $b);
-
-    $csrf_token = getNeedBetween($a, '/>' . PHP_EOL . '<meta content="', '" name="csrf-token"');
-
-    $post_data = [
-        'user[login]' => 'springjk',
-        'user[password]' => '445092433',
-        'user[remember_me]' => 1,
-        'authenticity_token' => $csrf_token,
-        'commit' => '登录',
-        'utf8' => '✓',
-    ];
-
-    $c = login_post($login_url, $cookie, $post_data);
-    $d = get_content('https://www.ytpub.com/admin/servers', $cookie);
-
-
-    $html = $d;
-
-
-    $html = eregi_replace(">[\r\n\t ]+<", "><", $html); // 去掉多余的空字符
-    eregi("<table[^>]*>(.+)</table>", $html, $regs); // 提取表体
-    $array = split("</tr>", $regs[1]); // 按行分解成数组
-    array_pop($array); // 去处尾部多余的元素
-    for ($i = 0; $i < count($array); $i++) {
-        $array[$i] = split("</td>", $array[$i]); // 分裂各列
-        array_pop($array[$i]); // 去处尾部多余的元素
     }
-    for ($i = 0; $i < count($array); $i++) {
-        for ($j = 0; $j < count($array[$j]); $j++) {
-            if (eregi("colspan.*([0-9]+)", $array[$i][$j], $regs)) { // 如果跨列
-                $t = array();
-                while (--$regs[1] > 0) // 补足差额
-                    array_push($t, "");
-                $array[$i] = array_merge(array_slice($array[$i], 0, $j + 1), $t, array_splice($array[$i], $j + 1));
-            }
-            if (eregi("rowspan.*([0-9]+)", $array[$i][$j], $regs)) { // 如果跨行
-                if (!isset($t)) // 跨列、跨行不同时存在
-                    $t = array("");
-                else
-                    array_push($t, "");
-                $k = $regs[1];
-                while (--$k > 0) // 补足差额
-                    $array[$i + $k] = array_merge(array_slice($array[$i + $k], 0, $j), $t, array_splice($array[$i + $k], $j));
-            }
-            unset($t);
+
+    public function getCsrfToken()
+    {
+        $result = $this->client->get(self::LOGIN_PATH);
+
+        $html = $result->getBody();
+
+        $html_dom = $this->parser->str_get_html($html);
+
+        $token = $html_dom->find('meta[name=csrf-token]', 0)->getAttribute('content');
+
+        if (empty($token)) {
+            throw new \Exception('Get csrf token error');
+        }
+
+        $this->token = $token;
+
+        return $token;
+    }
+
+    private function login($username, $password)
+    {
+        $this->getCsrfToken();
+
+        $result = $this->client->post(self::LOGIN_PATH, [
+            'form_params' => [
+                'user[login]' => $username,
+                'user[password]' => $password,
+                'user[remember_me]' => 1,
+                'authenticity_token' => $this->token,
+                'commit' => '登录',
+                'utf8' => '✓',
+            ]
+        ]);
+
+        $html = $result->getBody();
+
+        $html_dom = $this->parser->str_get_html($html);
+
+        $login_info = $html_dom->find('div.alert-success, div.alert-danger', 0)->plaintext;
+
+        # cut utf-8 chinese
+        preg_match_all('/[\x{4e00}-\x{9fa5}]+/u', $login_info, $matches);
+
+        $login_info = $matches[0][0];
+
+        if ($login_info == '登录成功') {
+            return true;
+        } else {
+            throw new \Exception($login_info);
         }
     }
 
-    // 除去html标记
-    for ($i = 0; $i < count($array); $i++) {
-        for ($j = 0; $j < count($array[$i]); $j++) {
-            $array[$i][$j] = trim(strip_tags($array[$i][$j]));
+    private function getServers()
+    {
+
+        $result = $this->client->get(self::SERVERS_PATH);
+
+        $html =  $result->getBody();
+
+        $html_dom = $this->parser->str_get_html($html);
+
+        $servers = [];
+
+        foreach ($html_dom->find('table tr') as $key => $tr) {
+            # ignore th
+            if ($key != 0) {
+                $name = trim($tr->find('td[rowspan=2]', -1)->plaintext);
+
+                if (empty($name)) {
+                    $name = $servers[$key-2]['name'];
+                }
+
+                $ip = trim($tr->find('td', -4)->plaintext);
+
+                $type = trim($tr->find('td', -2)->plaintext);
+
+                $servers[] = compact('name', 'ip', 'type');
+            }
         }
+        return $servers;
     }
 
-    $servers = array_filter($array);
-    $k = 0;
-    foreach ($servers as $key => $value) {
-        $count = count($value);
-        switch ($count) {
-            case 6:
-                $k = $key;
+    public function storageServers(array $servers)
+    {
+        $file = fopen('servers.txt', 'w') or die('Unable to write file!' . PHP_EOL);
+
+        fwrite($file, json_encode($servers));
+
+        fclose($file);
+    }
+
+    public function inputAuth()
+    {
+        do{
+            fwrite(STDOUT, '请输入您的云梯登陆账号：' . PHP_EOL);
+            $username = trim(fgets(STDIN));
+
+            if ($username) {
+                fwrite(STDOUT, '请输入您的云梯登陆密码：' . PHP_EOL);
+                $password = trim(fgets(STDIN));
+            } else {
+                fwrite(STDOUT, '抱歉，账号不能为空，请重新输入：' . PHP_EOL);
+            }
+
+            $auth = compact('username', 'password');
+        }while(!$auth);
+
+        return $auth;
+    }
+
+    public function saveAuth(array $auth)
+    {
+        $file = fopen('auth.txt', 'w') or die('Unable to write file!' . PHP_EOL);
+
+        fwrite($file, json_encode($auth));
+
+        fclose($file);
+    }
+
+    public function pingTest($servers)
+    {
+
+        foreach ($servers as $server) {
+            exec('ping -c 3 ' . $server['ip'] . ' &', $result);
+
+            // for windows
+            //$result = explode('= ', $result[10]);
+            // preg_match_all('/\d+/', $result[5], $arr);
+            // print_r($result);
+            // print_r($arr);
+            //echo $server[1].' '.$server[2].' '.$result[10],PHP_EOL;
+
+            // for mac
+            preg_match_all('/[1-9]\d*\.\d{3}/', end($result), $arr);
+
+            echo $server['name'] . '--' . $server['type'] . '--' . $server['ip'] . '--' . $arr[0][1] . PHP_EOL;
+
+            if (isset($arr[0][1])) {
+                $avg_array[] = $arr[0][1];
+            } else {
+                $avg_array[] = '无法连接';
+            }
+
+            if (min($avg_array) == $arr[0][1]) {
+                $fastest_vpn_name = '云梯 ' . $server['name'] . ' ' . $server['type'];
+                $fastest_vpn_info = '延迟最低的线路为：' . $fastest_vpn_name . ' AVG: ' . $arr[0][1];
+            }
+        }
+
+        $scpt_code_templete = [
+            'tell application "System Events"',
+            'tell current location of network preferences',
+            'set VPNservice to service "' . $fastest_vpn_name . '" -- name of the VPN service',
+            'if exists VPNservice then connect VPNservice',
+            'end tell',
+            'end tell',
+        ];
+        $scpt_code = 'osascript';
+        foreach ($scpt_code_templete as $key => $value) {
+            $scpt_code .= ' -e \'' . $value . '\'';
+        }
+
+        echo $this->colorize($fastest_vpn_info . PHP_EOL, "SUCCESS");
+
+        echo '正在连接' . $fastest_vpn_name . PHP_EOL;
+
+        exec($scpt_code, $result);
+
+        echo $this->colorize('完成' . PHP_EOL, "SUCCESS");
+
+        // 测速
+        // ping google.com
+        // down file
+
+    }
+
+
+    public function colorize($text, $status)
+    {
+        $out = '';
+        switch ($status) {
+            case "SUCCESS":
+                $out = "[32m"; //Green
                 break;
-            case 5:
-                array_unshift($servers[$key], $servers[$k][0]);
+            case "FAILURE":
+                $out = "[31m"; //Red
                 break;
-            case 4:
-                array_unshift($servers[$key], $servers[$k][1]);
-                array_unshift($servers[$key], $servers[$k][0]);
+            case "WARNING":
+                $out = "[33m"; //Yellow
+                break;
+            case "NOTE":
+                $out = "[34m"; //Blue
                 break;
             default:
-                // do noting
-                break;
+                throw new Exception("Invalid status: " . $status);
         }
-    }
-
-    $servers_stroge = [
-        'data' => $servers,
-        'expires' => time() + 3600 * 24 * 7,
-    ];
-
-    file_put_contents('servers.txt', json_encode($servers_stroge));
-
-} else {
-    $servers = $servers['data'];
-}
-
-foreach ($servers as $server) {
-    exec('ping -c 3 ' . $server[2], $result);
-
-    // for windows
-    //$result = explode('= ', $result[10]);
-    // preg_match_all('/\d+/', $result[5], $arr);
-    // print_r($result);
-    // print_r($arr);
-    //echo $server[1].' '.$server[2].' '.$result[10],PHP_EOL;
-
-    // for mac
-    preg_match_all('/[1-9]\d*\.\d{3}/', end($result), $arr);
-
-    echo $server[1] . '--' . $server[4] . '--' . $server[2] . '--' . $arr[0][1] . PHP_EOL;
-
-    if (isset($arr[0][1])) {
-        $avg_array[] = $arr[0][1];
-    } else {
-        $avg_array[] = '无法连接';
-    }
-
-    if (min($avg_array) == $arr[0][1]) {
-        $fastest_vpn_name = '云梯 ' . $server[1] . ' ' . $server[4];
-        $fastest_vpn_info = '延迟最低的线路为：' . $fastest_vpn_name . ' AVG: ' . $arr[0][1];
+        return chr(27) . "$out" . "$text" . chr(27) . "[0m";
     }
 }
 
-$scpt_code_templete = [
-    'tell application "System Events"',
-    'tell current location of network preferences',
-    'set VPNservice to service "' . $fastest_vpn_name . '" -- name of the VPN service',
-    'if exists VPNservice then connect VPNservice',
-    'end tell',
-    'end tell',
-];
-$scpt_code = 'osascript';
-foreach ($scpt_code_templete as $key => $value) {
-    $scpt_code .= ' -e \'' . $value . '\'';
-}
+$yunti = new YunTi();
 
-echo colorize($fastest_vpn_info . PHP_EOL, "SUCCESS");
-
-echo '正在连接' . $fastest_vpn_name . PHP_EOL;
-
-exec($scpt_code, $result);
-
-echo colorize('完成' . PHP_EOL, "SUCCESS");
+$yunti->run();
 
 // 测速
 // ping google.com
 // down file
-
